3b62c34f22e86e4e5bc56c7ff394aba35ba51e36
SofyanSaputra13 <sofyan.saputra@students.amikom.ac.id>
2019-12-21 13:43:36 +0700

    menambahkan file

---
+++ per3.py
@@ -0,0 +1,19 @@
+list1 = ['Kimia','Fisika', 1993, 2017]
+list2 = [1, 2, 3, 4, 5, 6, 7]
+print ("list1[0]   : ", list1[0])
+print ("list2[1:5] : ", list2[1:5],'\n')
+
+s = [1, 2, 3, 4, 5, 6]
+p = [10, 20, 30, 40, 50]
+print (sum(s))
+print (sum(p))
+c = s + p
+print (sum(c), '\n')
+
+tup1 = ('Kimia','Fisika', 1993, 2017)
+tup2 = (1, 2, 3, 4, 5, 6, 7)
+tup3 = (1,2)
+print ("list1[0]   : ", tup1[0])
+print ("list2[1:5] : ", tup2[1:5])
+print (tup3)
+del 
